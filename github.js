1f4c2adddc11a1ab93797706ab17e7d87adc3bc8
SashaBodrova <74468520+SashaBodrova@users.noreply.github.com>
2021-11-15 21:44:51 +0300

    1.0.0

---
+++ github.js
@@ -0,0 +1,24 @@
+class Github {
+		constructor() {
+				this.client_id = 'Iv1.766c339352bbf168';
+				this.client_secret = '065722fa14e1566142c8b0c4741c1508db727bb0';
+				this.repos_count = 5;
+				this.repos_sort = 'created: asc';
+		}
+
+		async getUser(user) {
+				// Get profile
+				const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+				// Get profile's repos
+				const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+				const profile = await profileResponse.json();
+				const repos = await repoResponse.json();
+
+				return {
+						profile,
+						repos
+				}
+		}
+}
---
+++ ui.js
@@ -0,0 +1,89 @@
+class UI {
+		constructor() {
+				this.profile = document.getElementById('profile');
+				this.resitories = document.querySelector('#repos');
+		}
+
+		showProfile(user) {
+				this.profile.innerHTML = `
+      <div class='card card-body mb-3'>
+        <div class='row'>
+          <div class='col-md-3'>
+            <img class='img-fluid mb-2' src="${user.avatar_url}">
+            <a href='${user.html_url}' target='_blank' class='btn btn-primary btn-block'>View Profile</a>
+          </div>
+          <div class='col-md-9'>
+            <span class="badge badge-primary">Public Repos: ${user.public_repos}</span>
+            <span class="badge badge-secondary">Public Gists: ${user.public_gists}</span>
+            <span class="badge badge-success">Followers: ${user.followers}</span>
+            <span class="badge badge-info">Following: ${user.following}</span>
+            <br><br>
+            <ul class="list-group">
+              <li class="list-group-item">Company: ${user.company}</li>
+              <li class="list-group-item">Website/Blog: ${user.blog}</li>
+              <li class="list-group-item">Location: ${user.location}</li>
+              <li class="list-group-item">Member Since: ${user.created_at}</li>
+            </ul>
+          </div>
+        </div>
+      </div>`;
+		}
+
+		showRepos(repos){
+				let output = '';
+
+				repos.forEach(function(repo) {
+						output += `
+        <div class="card card-body mb-2">
+          <div class="row">
+            <div class="col-md-6">
+              <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+            </div>
+            <div class="col-md-6">
+            <span class="badge badge-primary">Stars: ${repo.stargazers_count}</span>
+            <span class="badge badge-secondary">Watchers: ${repo.watchers_count}</span>
+            <span class="badge badge-success">Forks: ${repo.forms_count}</span>
+            </div>
+          </div>
+        </div>
+      `;
+				});
+				document.getElementById('repos').innerHTML = output;
+		}
+
+		showAlert(message, className) {
+				// If there is 1 alert, remove others
+				this.clearAlert();
+
+				// Create a div with class and text
+				const div = document.createElement('div');
+				div.className = className;
+				div.appendChild(document.createTextNode(message));
+
+				// Place where insert
+				const container = document.querySelector('.searchContainer');
+				const search = document.querySelector('.search');
+				container.insertBefore(div, search);
+
+				// Timeout for alert
+				setTimeout(() => {
+						this.clearAlert()
+				}, 3000)
+		}
+
+		// CLear alert
+		clearAlert() {
+				const currentAlert = document.querySelector('.alert');
+				if(currentAlert){
+						currentAlert.remove();
+				}
+		}
+
+		clearProfile(){
+				this.profile.innerHTML = '';
+		}
+
+		clearRepo(){
+				this.resitories.innerHTML = '';
+		}
+}
